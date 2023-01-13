e7c9ada3c6d1a3b84162c67f8b2aa152083e8778
Alvin-Karanja <alvin2@proton.me>
2023-01-13 18:26:08 +0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>RapidAPI - Lyrics App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,127 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import axios from "axios";
+import { useState } from "react";
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [title, setTitle] = useState("Alan Walker");
+  const [searchResults, setSearchResults] = useState(null);
+  const [lyrics, setLyrics] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getResults = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get("api/search/", {
+        params: { title },
+      });
+      const { data } = res;
+      setSearchResults(data.response.hits);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  const getLyrics = async id => {
+    try {
+      setSearchResults(null);
+      setLoading(true);
+      const res = await axios.get("api/lyrics/", {
+        params: { id },
+      });
+      const { data } = res;
+      setLoading(false);
+      setLyrics(data.response.lyrics);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col md:px-12 px-0 relative bg-background font-poppins items-center min-h-screen">
+      <h1 className="text-6xl font-bold text-primary mt-10">
+        <span className="text-active">Lyrics</span> App
+      </h1>
+      <h2 className="text-primary text-2xl font-light mt-6">
+        Get the complete lyrics of any given track.
+      </h2>
+      <form
+        className="sm:mx-auto mt-20 justify-center sm:w-full sm:flex"
+        onSubmit={(e) => {
+          getResults();
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <input
+          type="text"
+          className="flex w-full sm:w-1/3 rounded-lg px-5 py-3 text-base text-background font-semibold focus:outline-none focus:ring-2 focus:ring-active"
+          placeholder="Alan Walker"
+          onChange={(e) => {
+            setTitle(e.target.value);
+            setSearchResults(null);
+            setLyrics(null);
+          }}
+        />
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+        <div className="mt-4 sm:mt-0 sm:ml-3">
+          <button
+            className="block w-full rounded-lg px-5 py-3 bg-active text-base text-primary font-bold hover:text-active hover:bg-primary sm:px-10"
+            type="submit"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {loading ? <>Loading..</> : <>Search</>}
+          </button>
         </div>
-      </main>
+      </form>
+      {searchResults && (
+        <div className="mt-10">
+          <div className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {searchResults.map((song) => (
+              <div key={song.result.id} className="pt-6">
+                <div className="flow-root bg-light rounded-lg px-4 pb-8">
+                  <div className="-mt-6">
+                    <div className="flex items-center justify-center">
+                      <span className="p-2">
+                        <img
+                          src={song.result.song_art_image_thumbnail_url}
+                          className="w-full h-full rounded-lg"
+                          alt={song.result.song_art_image_thumbnail_url}
+                        />
+                      </span>
+                    </div>
+                    <div className="text-center justify-center items-center">
+                      <h3 className="mt-4 text-lg font-bold w-full break-words overflow-x-auto text-primary tracking-tight">
+                        {song.result.title}
+                      </h3>
+                      <span className="mt-2 text-sm text-secondary block">
+                        {song.result.artist_names}
+                      </span>
+                      <button
+                        className="mt-5 text-md text-active"
+                        onClick={() => {
+                          getLyrics(song.result.id);
+                        }}
+                      >
+                        Get Lyrics &rarr;
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      {lyrics && (
+        <div className="mt-10 max-w-2xl">
+          <h2 className="text-2xl font-bold text-center text-active">
+            Lyrics for {lyrics.trackingData.Title}
+          </h2>
+
+          <p className="mt-6 leading-loose text-primary text-xl">
+            {lyrics.lyrics.body.plain}
+          </p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
-
-export default Home
